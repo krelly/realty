48ba3d5ca67e381c6633069f67263632cf6b9a85
krelly <itwillnotbeasy@gmail.com>
2016-03-09 00:27:57 +0200

    Address input with autocomplete, map on /show action

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -69,6 +69,6 @@ class ApartmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def apartment_params
-      params.require(:apartment).permit(:description, :floor, :price, :rooms, :area)
+      params.require(:apartment).permit(:description, :floor, :price, :rooms, :area, :longitude, :latitude)
     end
 end
