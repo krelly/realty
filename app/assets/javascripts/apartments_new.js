1501a51df3c8129b327cf78009853589901dd101
krelly <itwillnotbeasy@gmail.com>
2016-03-17 19:44:13 +0200

    Fixed views autocomplete

--- app/assets/javascripts/apartments_new.js
+++ app/assets/javascripts/apartments_new.js
@@ -2,14 +2,39 @@ $('#new_apartment').ready(function() {
 
 })
 
-function onLoad(ymaps) {
-    var opts = {
-        provider: 'yandex#map'
-    }
-    var input = $('#apartment_address')
-    var suggestView = new ymaps.SuggestView('apartment_address');
-    suggestView.events.add('select', e => {
-        var address = e.get('item').value
-        input.val(address)
-    })
-}
+
+
+
+
+$(() => {
+    window.ymaps.ready(function() {
+        if ($(".apartments.new, .apartments.edit").length) {
+            console.log('here');
+            var input = $('#apartment_address')
+            var suggestView = new ymaps.SuggestView('apartment_address');
+            suggestView.events.add('select', e => {
+                var address = e.get('item').value
+                input.val(address)
+            })
+        }
+
+        if ($(".apartments.show").length) {
+            $map = $("#interactive-map")
+            lat = $map.data('latitude')
+            lng = $map.data('longitude')
+            myMap = new ymaps.Map("interactive-map", {
+                center: [lat, lng],
+                zoom: 16,
+                behaviors: ['default', 'scrollZoom']
+            });
+            var myPlacemark = new ymaps.Placemark([lat, lng]
+                /*, {
+                                balloonContentBody: ui.item.label,
+                                hintContent: ui.item.label
+                            }*/
+            );
+            myMap.geoObjects.add(myPlacemark);
+            // myMap.setCenter([longlat[1], longlat[0]], 13);
+        }
+    });
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require suggestView-2.1.js
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require suggestView-2.1.js
