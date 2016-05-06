30ff0b43bc86434cefe82a0c5407bc7bc7a9cb38
krelly <itwillnotbeasy@gmail.com>
2016-05-06 13:42:27 +0300

    Add helper which higlights links to current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path, options = nil)
+    link_hash = Rails.application.routes.recognize_path link_path
+    options[:class] << ' active' if current_page_and_action(link_hash)
+    link_to link_text, link_path, options
+  end
+
+  def current_page_and_action(controller:, action:)
+    if controller?(controller) && action?(action)
+      true
+    else
+      false
+    end
+  end
+
+  def controller?(*controller)
+    controller.include?(controller_name)
+  end
+
+  def action?(*action)
+   action.include?(action_name)
+  end
 end
